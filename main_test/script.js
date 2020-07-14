c43e819854022a4103c9d772975f271e91c722fa
fmoonblue <fmoonblue@gmail.com>
2020-07-14 16:28:05 +0900

    사이드 배너 추가

--- main_test/script.js
+++ main_test/script.js
@@ -41,3 +41,17 @@ $(function() {
 });
 // jQuery load
 
+
+/* side banner top click*/
+$( window ).scroll( function() {
+    if ( $( this ).scrollTop() > 200 ) {
+       $( '.top' ).fadeIn();
+    } else {
+       $( '.top' ).fadeOut();
+    }
+ } );
+ 
+ $( '.top' ).click( function() {
+    $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+    return false;
+ } );
